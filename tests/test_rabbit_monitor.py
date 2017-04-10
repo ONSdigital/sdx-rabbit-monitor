b632632ffb4d4263332927358e40458e7f0ef4b0
James Gardiner <james@jgardiner.co.uk>
2017-04-10 16:03:13 +0100

    Add tests for message counts

--- tests/test_rabbit_monitor.py
+++ tests/test_rabbit_monitor.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 
 import aiohttp
 from aiohttp import web
@@ -12,7 +13,7 @@ def cli(loop, test_client):
     app = web.Application()
     app.router.add_get('/healthcheck', self_healthcheck)
     app.router.add_get('/rabbit_aliveness_good', rabbit_fetch_good)
-    app.router.add_get('/test_message_count', message_count)
+    app.router.add_get('/test_message_count', mock_message_count)
     return loop.run_until_complete(test_client(app))
 
 
@@ -31,17 +32,32 @@ def rabbit_aliveness_good(request):
     return web.json_response({'status': 'ok'})
 
 
+@asyncio.coroutine
 def mock_message_count(request):
-    return web.json_response({'queue_totals': {'messages': 10}})
+    return web.json_response({'queue_totals': {
+                              'messages': 10,
+                              'messages_ready': 1,
+                              'messages_unacknowledged': 1,
+                              'messages_details': {
+                                  'rate': 0.0,
+                              }}},
+                             status=200)
 
 
 @asyncio.coroutine
-def test_message_count(cli):
+def test_message_counts(cli):
     port = cli.server.port
     url = 'http://localhost:{}/test_message_count'.format(port)
     session = aiohttp.ClientSession()
-    resp = message_count(session, url)
+    resp = yield from message_count(session, url)
     assert resp.status == 200
+    text = yield from resp.text()
+    text = json.loads(text)
+    queue_totals = text.get('queue_totals')
+    assert 10 == queue_totals.get('messages')
+    assert 0.0 == queue_totals.get('messages_details', {}).get('rate')
+    assert 1 == queue_totals.get('messages_ready')
+    assert 1 == queue_totals.get('messages_unacknowledged')
 
 
 @asyncio.coroutine
