19d256ffa9f8a459e0db509d02cf9439969992e6
Warren Bailey <warren@warrenbailey.net>
2017-04-11 14:57:11 +0100

    Adding disk space and memory usage check

--- tests/test_rabbit_monitor.py
+++ tests/test_rabbit_monitor.py
@@ -5,7 +5,8 @@ import aiohttp
 from aiohttp import web
 import pytest
 
-from rabbit_monitor import fetch, self_healthcheck, message_count
+
+from rabbit_monitor import fetch, self_healthcheck, message_count, nodes_info
 
 
 @pytest.fixture
@@ -14,6 +15,7 @@ def cli(loop, test_client):
     app.router.add_get('/healthcheck', self_healthcheck)
     app.router.add_get('/rabbit_aliveness_good', rabbit_fetch_good)
     app.router.add_get('/test_message_count', mock_message_count)
+    app.router.add_get('/test_nodes', mock_nodes)
     return loop.run_until_complete(test_client(app))
 
 
@@ -58,6 +60,31 @@ def test_message_counts(cli):
     assert 0.0 == queue_totals.get('messages_details', {}).get('rate')
     assert 1 == queue_totals.get('messages_ready')
     assert 1 == queue_totals.get('messages_unacknowledged')
+
+
+@asyncio.coroutine
+def mock_nodes(request):
+    return web.json_response([{"name": "rabbit@76b7837eff57",
+                               "mem_limit": 838474137,
+                               "disk_free_limit": 50000000,
+                               "mem_used": 77209008,
+                               "disk_free": 58268864512}],
+                             status=200)
+
+
+@asyncio.coroutine
+def test_node_info(cli):
+    port = cli.server.port
+    url = 'http://localhost:{}/test_nodes'.format(port)
+    session = aiohttp.ClientSession()
+    name, free_disk_space, free_disk_space_limit, percent_disk, memory_used, memory_used_limit, percent_mem = yield from nodes_info(session, url)
+    assert "rabbit@76b7837eff57" == name
+    assert 58268864512 == free_disk_space
+    assert 77209008 == memory_used
+    assert 50000000 == free_disk_space_limit
+    assert 838474137 == memory_used_limit
+    assert '99.91%' == percent_disk
+    assert '90.79%' == percent_mem
 
 
 @asyncio.coroutine
