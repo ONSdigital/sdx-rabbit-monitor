c8d5ddf1683d9b7d7b11f9ae7f196c7d9573251a
Adam Mann <manny160389@hotmail.com>
2017-10-13 15:29:36 +0100

    Add defaults to settings so less config is required

--- settings.py
+++ settings.py
@@ -4,21 +4,21 @@ import os
 LOGGING_FORMAT = "%(asctime)s|%(levelname)s: sdx-rabbit-monitor: %(message)s"
 LOGGING_LEVEL = logging.getLevelName(os.getenv('LOGGING_LEVEL', 'DEBUG'))
 
-PORT = os.getenv('SDX_RABBIT_MONITOR_HOST')
+PORT = os.getenv('SDX_RABBIT_MONITOR_PORT')
 
 RABBITMQ_DEFAULT_PASS = os.getenv('SDX_RABBIT_MONITOR_PASS')
 RABBITMQ_DEFAULT_USER = os.getenv('SDX_RABBIT_MONITOR_USER')
-RABBITMQ_DEFAULT_VHOST = os.getenv('SDX_RABBIT_MONITOR_DEFAULT_VHOST')
+RABBITMQ_DEFAULT_VHOST = os.getenv('SDX_RABBIT_MONITOR_DEFAULT_VHOST', '%2f')
 
 RABBIT_URL = 'http://{hostname}:{port}/api/'.format(
     hostname=os.getenv('SDX_RABBIT_MONITOR_RABBIT_HOST'),
     port=os.getenv('SDX_RABBIT_MONITOR_MGT_PORT')
 )
 
-WAIT_TIME = int(os.getenv('SDX_RABBIT_MONITOR_WAIT_TIME'))
+WAIT_TIME = int(os.getenv('SDX_RABBIT_MONITOR_WAIT_TIME', '120'))
 
 # Number of seconds to look back and gather stats from
-RABBIT_MONITOR_STATS_WINDOW = int(os.getenv('SDX_RABBIT_MONITOR_STATS_WINDOW'))
+RABBIT_MONITOR_STATS_WINDOW = int(os.getenv('SDX_RABBIT_MONITOR_STATS_WINDOW', '120'))
 
 # Sample frequency in stats window
-RABBIT_MONITOR_STATS_INCREMENT = os.getenv('SDX_RABBIT_MONITOR_STATS_INCREMENT')
+RABBIT_MONITOR_STATS_INCREMENT = os.getenv('SDX_RABBIT_MONITOR_STATS_INCREMENT', '30')
