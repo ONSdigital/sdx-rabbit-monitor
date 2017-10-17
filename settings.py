7da63dca09b9e7e5c4f070fcc12526a71c616419
Adam Mann <manny160389@hotmail.com>
2017-10-17 08:56:19 +0100

    Remove unused configurable settings

--- settings.py
+++ settings.py
@@ -8,17 +8,17 @@ PORT = os.getenv('SDX_RABBIT_MONITOR_PORT')
 
 RABBITMQ_DEFAULT_PASS = os.getenv('SDX_RABBIT_MONITOR_PASS')
 RABBITMQ_DEFAULT_USER = os.getenv('SDX_RABBIT_MONITOR_USER')
-RABBITMQ_DEFAULT_VHOST = os.getenv('SDX_RABBIT_MONITOR_DEFAULT_VHOST', '%2f')
+RABBITMQ_DEFAULT_VHOST = '%2f'
 
 RABBIT_URL = 'http://{hostname}:{port}/api/'.format(
     hostname=os.getenv('SDX_RABBIT_MONITOR_RABBIT_HOST'),
     port=os.getenv('SDX_RABBIT_MONITOR_MGT_PORT')
 )
 
-WAIT_TIME = int(os.getenv('SDX_RABBIT_MONITOR_WAIT_TIME', '120'))
+WAIT_TIME = int('120')
 
 # Number of seconds to look back and gather stats from
-RABBIT_MONITOR_STATS_WINDOW = int(os.getenv('SDX_RABBIT_MONITOR_STATS_WINDOW', '120'))
+RABBIT_MONITOR_STATS_WINDOW = int('120')
 
 # Sample frequency in stats window
-RABBIT_MONITOR_STATS_INCREMENT = os.getenv('SDX_RABBIT_MONITOR_STATS_INCREMENT', '30')
+RABBIT_MONITOR_STATS_INCREMENT = '30'
